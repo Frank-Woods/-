1277913a50dbbade22caab0b8b71cc3063a0a0e0
Frank-Woods <49346787+Frank-Woods@users.noreply.github.com>
2020-04-15 13:46:27 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -34,3 +34,52 @@ save.onclick = () => {
 	localStorage.setItem('date', date.value);
 	window.location.replace('index.html');
 }
+
+const inpOninput = document.getElementById('inpOninput');
+const inpOnchange = document.getElementById('inpOnchange');
+const background = document.getElementById('background');
+const btnOnfocusOnblur = document.getElementById('btnOnfocusOnblur');
+const inpOnkeydown = document.getElementById('inpOnkeydown');
+
+background.onclick = () => {
+	if (document.body.style.backgroundColor == "") {
+		document.body.style.backgroundColor = "red";
+	}
+	else {
+		document.body.style.backgroundColor = "white";
+	}
+}
+
+inpOnchange.onchange = () => {
+	login.value = " ";
+	password.value = " ";
+	firstName.value = " ";
+	lastName.value = " ";
+	date.value = " ";
+}
+
+btnOnfocusOnblur.onfocus = () => {
+	var img = document.getElementById('img');
+	img.style.transform = "rotate(15deg)";
+}
+
+btnOnfocusOnblur.onblur = () => {
+	var img = document.getElementById('img');
+	img.style.transform = "rotate(0deg)";
+}
+
+inpOninput.oninput = () => {
+    input.textContent = inpOninput.value;
+}
+
+inpOnkeydown.onkeydown = (e) => {
+    inpOnkeydown.value = '';
+    keyEvent.textContent = e.code;
+    keyName.textContent = 'Вы нажали клавишу: ';
+}
+
+inpOnkeydown.onkeyup = () => {
+    inpOnkeydown.value = '';
+    keyEvent.textContent = '';
+    keyName.textContent = 'Вы нажали клавишу: ';
+}
